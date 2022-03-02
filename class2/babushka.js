f3374a3771c8016f80baa9b7d36e29544532f622
DESKTOP-9CO10L2\Arnon <arnon.yaar@gmail.com>
2022-03-02 09:00:48 +0200

    now welcome

---
+++ class2/babushka.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(100, 100, 60);
+  circle(100, 100, 50);
+  circle(100, 100, 40);
+  circle(100, 100, 30);
+  circle(100, 100, 20);
+  circle(100, 100, 10);
+  
+  
+  rect(170,170, 60,40);
+  rect(170,170, 55,35);
+  rect(170,170, 50,30);
+  rect(170,170, 45,25);
+  
+}
