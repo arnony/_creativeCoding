4e5f7a9e2e2dff7b73c25dc07e2aa532f5780df9
DESKTOP-9CO10L2\Arnon <arnon.yaar@gmail.com>
2022-03-07 15:23:05 +0200

    lesson2 - summary

--- class2/babushka.js
+++ class2/babushka.js
@@ -1,3 +1,4 @@
+//Babushka of circles and rects + lines coming from the top left corner and spreading to the right side of the canvas
 function setup() {
   createCanvas(400, 400);
 }
@@ -10,11 +11,23 @@ function draw() {
   circle(100, 100, 30);
   circle(100, 100, 20);
   circle(100, 100, 10);
-  
-  
-  rect(170,170, 60,40);
-  rect(170,170, 55,35);
-  rect(170,170, 50,30);
-  rect(170,170, 45,25);
-  
+
+  rect(150, 150, 100, 80);
+  rect(160, 160, 80, 60);
+  rect(170, 170, 60, 40);
+  rect(180, 180, 40, 20);
+
+  line(10, 10, 370, 10);
+  line(10, 12, 370, 30);
+  line(10, 14, 370, 50);
+  line(10, 16, 370, 70);
+  line(10, 18, 370, 90);
+  line(10, 20, 370, 110);
+  line(10, 22, 370, 130);
+  line(10, 24, 370, 150);
+  line(10, 26, 370, 170);
+  line(10, 28, 370, 190);
+  line(10, 30, 370, 210);
+  line(10, 32, 370, 230);
+  line(10, 34, 370, 250);
 }
